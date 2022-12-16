0e8e3b5b39eb9dddf946731c8ccbecd082584a45
Jakub Smejkal <smejkal.jakub99@gmail.com>
2022-12-16 13:15:44 +0100

    Add load blocks and categories

--- blocks_generator.js
+++ blocks_generator.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const yaml = require('js-yaml');
 
 class BlockGenerator {
     constructor() {
@@ -18,11 +19,22 @@ class BlockGenerator {
 
     load_modules() {
         fs.readdirSync(this.modules_path).forEach(file => {
-            const module = require(path.join(this.modules_path, file));
-            this.modules[module.name] = module;
+            let file_name = file.split('.')[0];
+            let module = yaml.load(fs.readFileSync(path.join(this.modules_path, file), 'utf8'));
+            console.log(file_name)
+            this.modules[file_name] = module[file_name];
         });
+        console.log(this.modules); 
+    }
 
-        console.log(this.modules);
+    load_categories() {
+        fs.readdirSync(this.categories_path).forEach(file => {
+            let file_name = file.split('.')[0];
+            let category = yaml.load(fs.readFileSync(path.join(this.categories_path, file), 'utf8'));
+            console.log(file_name)
+            this.categories[file_name] = category[file_name];
+        });
+        console.log(this.categories); 
     }
 }
 
